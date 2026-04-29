1498f39899d1f1fc37300607ac605ea3fe01123f
Purnima <pverma@uvic.ca>
2026-04-28 21:52:21 -0700

    initial commit

---
+++ snake-game.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import random
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def charge_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
