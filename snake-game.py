83423b521f3d0b8799e5f54ffc50058dffdc3dac
Purnima <pverma@uvic.ca>
2026-04-29 11:22:41 -0700

    Constants and their descriptions are added

--- snake-game.py
+++ snake-game.py
@@ -13,14 +13,33 @@ import tkinter as tk
 import random
 
 """
-The 4 variables that are defined here are used to set up the
-game window and game speed. Since this is a snake game, the
+The 4 variables that are defined here are constants. These are
+values that are not to be changed and and are kind of like the
+game's settings. 
+The first variable that we have is 'GAME_WIDTH'which is set to 
+700. This variable defines the width of the game window in pixels.
 
+The second variable is the 'GAME_HEIGHT' which is also set to 
+700. This variable defines the height of the game window in
+pixels.
+
+The third variable is 'SPEED' which is set to 100. This variable 
+is used to control the speed of the snake in the game. The lower
+the number, the faster the snake is. The higher the number, the
+slower the snake is.
+
+The fourth variable is 'SPACE_SIZE' which is set to 50. This
+variable defines the size of each item in the game, such as the
+food that the snake eats and the segments of the snake's body.
+
+The fifth variable is 'BODY_PARTS' which is set to 3. This 
+variable defines the number of 
 """
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 100
 SPACE_SIZE = 50
+BODY_PARTS = 3
 
 class Snake:
     pass
